8bd4a029721065072a6a17d5a5daa57d576f4d91
pliffisson <pliffissongomes@gmail.com>
2020-07-16 16:29:04 -0400

    Desafio 4-1: Header

---
+++ desafio.4-1/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+const routes = require("./routes");
+
+const server = express();
+
+// ============ Usando Middlewares ===============
+server.use(express.static("public")); // Carregando a estilização da pagina
+server.use(routes);
+
+// =============== Usando o template engine ==========
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+  express: server,
+  autoescape: false,
+  noCache: true,
+});
+
+// ======== Servidor ===========
+server.listen(3000, function () {
+  console.log("server is running");
+});
